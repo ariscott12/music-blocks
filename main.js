0ff4d4beb353b5c69277cd816180cce8626a151e
Ari Scott <ariscott12@gmail.com>
2014-11-17 21:28:47 -0700

    removed random generating of blocks and added functionality to add blocks to grid area

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function musicBlock (w,h,x,y) {
 }
 
 musicBlock.prototype.initBlock = function(el) {
-	var section = document.getElementById("main")
+	var section = document.getElementById("main");
 	var node=document.createElement("LI");
 	node.setAttribute("class", "block");
 	section.appendChild(node);
@@ -27,20 +27,44 @@ musicBlock.prototype.makeBlock = function() {
 	setStyle(this.id, {'top':this.posY+"px", 'left':this.posX+"px",'width':this.width+"px", 'height':this.height+"px"});
 }
 
-var initialize = function() {
-	var accounts = [];
-	var randnums = [];
-	var myArguments = arguments[0];
-	for (var i = 0; i < 100; i++) {
-		for (var j = 0; j < 4; j++) {
-			if(j < 2) {
-				randnums[j] = Math.floor(Math.random()*myArguments.size);
-			} else {
-				randnums[j] = Math.floor(Math.random()*myArguments.position);
-			}
-		}
-		accounts[i] = new musicBlock(randnums[0],randnums[1],randnums[2],randnums[3]);
-		accounts[i].initBlock("block"+i).makeBlock();
-	};
 
-}({"size":25,"position":500});
+
+var grid = function() {
+	var section = document.getElementById("main");
+	var cnt = 0;
+	section.addEventListener("click", printMousePos);
+	
+	function printMousePos(e) {
+	    var cursorX = e.pageX;
+    	var cursorY = e.pageY;
+	    var myBlock = new musicBlock(20,20,Math.ceil((e.pageX-25) / 25)*25, Math.ceil((e.pageY-25) / 25)*25);
+	    myBlock.initBlock("block"+cnt).makeBlock();
+	    cnt++;
+	    console.log("X: " + e.pageX + " Y: " + e.pageY);
+	}
+}();
+
+
+
+
+
+
+
+
+// var initialize = function() {
+// 	var accounts = [];
+// 	var randnums = [];
+// 	var myArguments = arguments[0];
+// 	for (var i = 0; i < 100; i++) {
+// 		for (var j = 0; j < 4; j++) {
+// 			if(j < 2) {
+// 				randnums[j] = Math.floor(Math.random()*myArguments.size);
+// 			} else {
+// 				randnums[j] = Math.floor(Math.random()*myArguments.position);
+// 			}
+// 		}
+// 		accounts[i] = new musicBlock(randnums[0],randnums[1],randnums[2],randnums[3]);
+// 		accounts[i].initBlock("block"+i).makeBlock();
+// 	};
+
+// }({"size":25,"position":500});
