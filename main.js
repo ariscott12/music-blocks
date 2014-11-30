a083fe9d813f035eacbb55d5ab3e7ec01595b262
Ari Scott <ariscott12@gmail.com>
2014-11-30 07:41:41 -0700

    updated request animation frame basic click and animate functionality  working

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function musicBlock (w,h,x,y) {
 	this.posX = x;
 	this.posY = y;
 	this.id = "";
+	this.direction= 5;
 }
 
 musicBlock.prototype.initBlock = function(el) {
@@ -28,21 +29,82 @@ musicBlock.prototype.makeBlock = function() {
 }
 
 
+ var currentPos = 5;
+// musicBlock.prototype.animateBlock = function() {
+// 	currentPos +=5;
+// 	console.log(currentPos);
+
+// 	// function setStyle( objId, propertyObject )
+// 	// {
+// 	// 	 var elem = document.getElementById(objId);
+// 	// 	 for (var property in propertyObject)
+// 	// 	    elem.style[property] = propertyObject[property];
+// 	// }
+// 	// setStyle(this.id, {'top':this.posY+currentPos+"px", 'left':this.posX+"px",'width':this.width+"px", 'height':this.height+"px"});
+// 	var that = this;
+// 	requestAnimationFrame(that.animateBlock()); 
+// }
+
+
+function repeatOften(obj) {
+  currentPos +=5;
+  requestAnimationFrame(repeatOften);
+
+	
+
+  // console.log(myBlock.posY);
+   function setStyle( objId, propertyObject )
+	{
+		 var elem = document.getElementById(objId);
+		 for (var property in propertyObject)
+		    elem.style[property] = propertyObject[property];
+	}
+	for (var i = 0; i < objs.length; i++) {
+		
+		setStyle(objs[i].id, {'top':objs[i].posY+"px"});
+		objs[i].posY += objs[i].direction;
+
+		if(objs[i].posY == 500) {
+			objs[i].direction = -5;
+		}
+		if(objs[i].posY == 0) {
+			objs[i].direction = 5;
+		}
+		
+	};
+	//setStyle(objs[cnt].id, {'top':objs[cnt].posY+currentPos+"px"});
+}
+
+
+
+var going = true;
 
 var grid = function() {
 	var section = document.getElementById("main");
 	var cnt = 0;
+	
+	objs = [];
 	section.addEventListener("click", printMousePos);
 	
 	function printMousePos(e) {
+		console.log(cnt);
 	    var cursorX = e.pageX;
     	var cursorY = e.pageY;
-	    var myBlock = new musicBlock(20,20,Math.ceil((e.pageX-25) / 25)*25, Math.ceil((e.pageY-25) / 25)*25);
-	    myBlock.initBlock("block"+cnt).makeBlock();
-	    cnt++;
-	    console.log("X: " + e.pageX + " Y: " + e.pageY);
+	    objs[cnt] = new musicBlock(20,20,Math.ceil((e.pageX-25) / 25)*25, Math.ceil((e.pageY-25) / 25)*25);
+	    objs[cnt].initBlock("block"+cnt).makeBlock();
+	 	 
+	 	 if(going) {
+	 		repeatOften();
+	 		going = false;
+		 }
+		 	
+	   cnt++;
+	    
+	
 	}
 }();
+
+
 
 
 
