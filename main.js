8085380d1b06dab0aa96ddbce648bc3e6ff17a61
Ari Scott <ariscott12@gmail.com>
2014-11-30 18:10:56 -0700

    basic hit detection added

--- main.js
+++ main.js
@@ -1,132 +1,94 @@
-function musicBlock (w,h,x,y) {
-	this.width = w;
-	this.height = h;
-	this.posX = x;
-	this.posY = y;
-	this.id = "";
-	this.direction= 5;
+function musicBlock(w, h, x, y) {
+    this.width = w;
+    this.height = h;
+    this.posX = x;
+    this.posY = y;
+    this.id = "";
+    this.direction = 1	;
+    this.speed = 5;
+}
+
+function setStyle(objId, propertyObject) {
+    var elem = document.getElementById(objId);
+    for (var property in propertyObject)
+        elem.style[property] = propertyObject[property];
 }
 
 musicBlock.prototype.initBlock = function(el) {
-	var section = document.getElementById("main");
-	var node=document.createElement("LI");
-	node.setAttribute("class", "block");
-	section.appendChild(node);
-	this.id = el;
-	node.setAttribute("id", this.id);
-	return this;
-	
+    var section = document.getElementById("main");
+    var node = document.createElement("LI");
+    node.setAttribute("class", "block");
+    section.appendChild(node);
+    this.id = el;
+    node.setAttribute("id", this.id);
+    return this;
 };
 
 musicBlock.prototype.makeBlock = function() {
-	function setStyle( objId, propertyObject )
-	{
-		 var elem = document.getElementById(objId);
-		 for (var property in propertyObject)
-		    elem.style[property] = propertyObject[property];
-	}
-	setStyle(this.id, {'top':this.posY+"px", 'left':this.posX+"px",'width':this.width+"px", 'height':this.height+"px"});
-}
+    setStyle(this.id, {
+        'top': this.posY + "px",
+        'left': this.posX + "px",
+        'width': this.width + "px",
+        'height': this.height + "px"
+    });
+};
 
-
- var currentPos = 5;
-// musicBlock.prototype.animateBlock = function() {
-// 	currentPos +=5;
-// 	console.log(currentPos);
-
-// 	// function setStyle( objId, propertyObject )
-// 	// {
-// 	// 	 var elem = document.getElementById(objId);
-// 	// 	 for (var property in propertyObject)
-// 	// 	    elem.style[property] = propertyObject[property];
-// 	// }
-// 	// setStyle(this.id, {'top':this.posY+currentPos+"px", 'left':this.posX+"px",'width':this.width+"px", 'height':this.height+"px"});
-// 	var that = this;
-// 	requestAnimationFrame(that.animateBlock()); 
-// }
-
-
-function repeatOften(obj) {
-  currentPos +=5;
-  requestAnimationFrame(repeatOften);
-
-	
-
-  // console.log(myBlock.posY);
-   function setStyle( objId, propertyObject )
-	{
-		 var elem = document.getElementById(objId);
-		 for (var property in propertyObject)
-		    elem.style[property] = propertyObject[property];
-	}
-	for (var i = 0; i < objs.length; i++) {
-		
-		setStyle(objs[i].id, {'top':objs[i].posY+"px"});
-		objs[i].posY += objs[i].direction;
-
-		if(objs[i].posY == 500) {
-			objs[i].direction = -5;
-		}
-		if(objs[i].posY == 0) {
-			objs[i].direction = 5;
-		}
-		
-	};
-	//setStyle(objs[cnt].id, {'top':objs[cnt].posY+currentPos+"px"});
-}
-
-
-
-var going = true;
 
 var grid = function() {
-	var section = document.getElementById("main");
-	var cnt = 0;
-	
-	objs = [];
-	section.addEventListener("click", printMousePos);
-	
-	function printMousePos(e) {
-		console.log(cnt);
-	    var cursorX = e.pageX;
-    	var cursorY = e.pageY;
-	    objs[cnt] = new musicBlock(20,20,Math.ceil((e.pageX-25) / 25)*25, Math.ceil((e.pageY-25) / 25)*25);
-	    objs[cnt].initBlock("block"+cnt).makeBlock();
-	 	 
-	 	 if(going) {
-	 		repeatOften();
-	 		going = false;
-		 }
-		 	
-	   cnt++;
-	    
-	
-	}
+    var section = document.getElementById("main");
+    var cnt = 0;
+    var objs = [];
+    var running = true;
+
+
+    function animateBlocks() {
+        for (var i = 0; i < objs.length; i++) {
+            setStyle(objs[i].id, {
+                'top': objs[i].posY + "px"
+            });
+         
+            if (objs[i].posY >= 475 || objs[i].posY === 0 ) {
+                objs[i].direction = -objs[i].direction;
+            }
+            objs[i].posY += objs[i].direction * objs[i].speed;
+
+            for (var j = 0; j < objs.length; j++) {
+                if (i !== j) {
+                    if (objs[i].posY === objs[j].posY) {
+                        console.log("test");
+                    }
+                }
+            }
+        }
+        requestAnimationFrame(animateBlocks);
+    }
+
+    function setBlockPos(e) {
+        var cursorX = e.pageX;
+        var cursorY = e.pageY;
+        objs[cnt] = new musicBlock(25, 25, Math.ceil((e.pageX - 25) / 25) * 25, Math.ceil((e.pageY - 25) / 25) * 25);
+        objs[cnt].initBlock("block" + cnt).makeBlock();
+
+        if (running) {
+            animateBlocks();
+            running = false;
+        }
+        cnt++;
+    }
+    section.addEventListener("click", setBlockPos);
 }();
 
+var makeGrid = (function() {
+
+    for (var i = 0; i < 20; i++) {
+        var section = document.getElementById("gridHorizontal");
+        var section2 = document.getElementById("gridVertical");
+        var node = document.createElement("LI");
+        var node2 = document.createElement("LI");
+        section.appendChild(node);
+        section2.appendChild(node2);
+    }
 
 
 
-
-
-
-
-
-
-// var initialize = function() {
-// 	var accounts = [];
-// 	var randnums = [];
-// 	var myArguments = arguments[0];
-// 	for (var i = 0; i < 100; i++) {
-// 		for (var j = 0; j < 4; j++) {
-// 			if(j < 2) {
-// 				randnums[j] = Math.floor(Math.random()*myArguments.size);
-// 			} else {
-// 				randnums[j] = Math.floor(Math.random()*myArguments.position);
-// 			}
-// 		}
-// 		accounts[i] = new musicBlock(randnums[0],randnums[1],randnums[2],randnums[3]);
-// 		accounts[i].initBlock("block"+i).makeBlock();
-// 	};
-
-// }({"size":25,"position":500});
+})();
